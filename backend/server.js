6533fcf7b796bb7e2b9b11da54d199a195aedcfd
Siddharthmishraweb <72328255+Siddharthmishraweb@users.noreply.github.com>
2023-08-30 22:50:32 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://localhost/movie-recommendation', {
+mongoose.connect('mongodb+srv://mishrasiddharth1999:Reenter2@movie-cluster.oqg8uqt.mongodb.net/?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
