df7c41f573da71d1ff0b19b5d8a9113404104391
Siddharthmishraweb <72328255+Siddharthmishraweb@users.noreply.github.com>
2023-08-31 08:17:14 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+app.use(cors({ origin: '*' }));
 
 app.use(bodyParser.json());
 
